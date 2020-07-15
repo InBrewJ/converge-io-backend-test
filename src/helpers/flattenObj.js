8dacd36ce5eeaf68ba037b246d846768193a1da7
InBrewJ <jason@urawizard.com>
2020-07-15 16:10:48 +0100

    Added tests + working solution for the flatten object exercise

---
+++ src/helpers/flattenObj.js
@@ -0,0 +1,26 @@
+const flattenObj = (obj, keyQueue = [], accObj = {}) => {
+  // go through the keys with object keys
+  // for each key, recursively see if that has any keys
+  // store each of the string values key in a queue
+  // then join values in the keyQueue if the last value isn't an object
+
+  Object.keys(obj).forEach((key) => {
+    const currentValue = obj[key]
+    const isNotObject = typeof currentValue !== 'object'
+    const isArray = Array.isArray(currentValue)
+
+    if (isNotObject || isArray) {
+      // then we have a value like a array etc
+      const newKey = [...keyQueue, key].join('.')
+      accObj[newKey] = currentValue
+    } else {
+      flattenObj(obj[key], [...keyQueue, key], accObj)
+    }
+  })
+
+  return accObj
+}
+
+module.exports = {
+  flattenObj
+}
