f9c97634b0fa82e9ec6d8acaaaeb88d15eae45e0
InBrewJ <jason@urawizard.com>
2020-07-16 07:30:21 +0100

    refactored flattenObj very slightly and made variable naming slightly clearer

--- src/helpers/flattenObj.js
+++ src/helpers/flattenObj.js
@@ -4,17 +4,21 @@ const flattenObj = (obj, keyQueue = [], accObj = {}) => {
   // store each of the string values key in a queue
   // then join values in the keyQueue if the last value isn't an object
 
-  Object.keys(obj).forEach((key) => {
+  Object.keys(obj).map((key) => {
     const currentValue = obj[key]
-    const isNotObject = typeof currentValue !== 'object'
-    const isArray = Array.isArray(currentValue)
+    const keyIsNotObject = !!(typeof currentValue !== 'object')
+    const keyIsArray = !!Array.isArray(currentValue)
+    const prefixLog = [...keyQueue, key]
 
-    if (isNotObject || isArray) {
-      const newKey = [...keyQueue, key].join('.')
+    // Base case - we've reach a 'value'
+    // at the end of some nested keys
+    if (keyIsNotObject || keyIsArray) {
+      const newKey = prefixLog.join('.')
       accObj[newKey] = currentValue
-    } else {
-      flattenObj(obj[key], [...keyQueue, key], accObj)
+      return
     }
+
+    flattenObj(obj[key], prefixLog, accObj)
   })
 
   return accObj
